5b8001da9cc5c04b02ffbf9fa6a6ec085da7d7f5
AaronKWalker <aaron.walker@outlook.com>
2017-07-14 12:47:12 -0500

    futher changes to test2

--- testing/test2.js
+++ testing/test2.js
@@ -1,5 +1,13 @@
+
+
 //-------------------[put empty map on page upon load]-------------------//
 var testLatLong = {lat: 30.2471972222, lng: -97.750725}; //Austin, TX
+
+function callMe(){
+  initMap();
+  initMap2();
+}
+
 
 //creates blank map
 function initMap() {
@@ -92,3 +100,34 @@ $("#testBtn").on("click", function () {
 
 
 //-------------------[test: get directions]-------------------//
+
+
+function initMap2 () {
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map2 = new google.maps.Map(document.getElementById('map2'), {
+          zoom: 7,
+          center: {lat: 41.85, lng: -87.65}
+        });
+        directionsDisplay.setMap(map);
+
+        var onChangeHandler = function() {
+          calculateAndDisplayRoute(directionsService, directionsDisplay);
+        };
+        document.getElementById('start').addEventListener('change', onChangeHandler);
+        document.getElementById('end').addEventListener('change', onChangeHandler);
+      }
+
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        directionsService.route({
+          origin: document.getElementById('start').value,
+          destination: document.getElementById('end').value,
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
