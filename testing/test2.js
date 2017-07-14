bf67d60849918f220ea1562ece9eac138b4cbf61
Aaron Walker <p0ss1m.27@gmail.com>
2017-07-14 17:38:22 -0500

    creating code to combind all google code done so far '

--- testing/test2.js
+++ testing/test2.js
@@ -6,6 +6,7 @@ var testLatLong = {lat: 30.2471972222, lng: -97.750725}; //Austin, TX
 function callMe(){
   initMap();
   initMap2();
+  initMap3();
 }
 
 
@@ -37,6 +38,8 @@ var testURL = "https://developers.zomato.com/api/v2.1/search?entity_id=278&entit
 //2. location type [entity_type] (see #1)
 //3. cuisines
 //NOTE: I feel that due to the limiations of zomato, it might be easier if the user can only put in a zip code and select a cuisine from a dropdown menue.  Then the only ajax call (other then the one like the example below) would have to be the one that returns a location ID based on the zip code searched for.
+
+
 
 //lat & long variables
 var latitude;
@@ -136,3 +139,88 @@ function initMap2 () {
           }
         });
       }
+
+
+
+
+
+
+//-------------------[test: bring it all together]-------------------//
+
+
+function initMap3 () {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map3 = new google.maps.Map(document.getElementById('map3'), {
+    zoom: 7,
+    center: testLatLong
+  });
+  directionsDisplay.setMap(map2);
+
+  var onChangeHandler = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+}//-end of initMap3
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: document.getElementById('start').value,
+    destination: document.getElementById('end').value,
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
+
+
+//test button starts test
+$("#testBtn3").on("click", function () {
+  console.log("click!");
+
+  $.ajax({
+    url: testURL,
+    method: "GET",
+    headers:{
+      "user-key":zoAPI
+    }
+  })
+  //after getting the response
+  .done(function(response) {
+    console.log(response);
+
+    var map2 = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,//zoom set to 12 so all markers are seen at once
+      center: testLatLong
+    });
+
+    //for each of the objects returned
+
+    for (var m =0; m < response.restaurants.length; m ++){
+
+      latitude = response.restaurants[m].restaurant.location.latitude;
+      latitude = parseFloat(latitude);
+      longitude = response.restaurants[m].restaurant.location.longitude;
+      longitude = parseFloat(longitude);
+      console.log(latitude);
+      console.log(longitude);
+      console.log("SEPERATOR");
+
+
+      //create a marker for each object returned
+      var phiLambda = {lat: latitude, lng: longitude};
+
+      var marker = new google.maps.Marker({
+        position: phiLambda,
+        map: map2
+      });
+      markerClick(marker);
+      console.log(marker);
+      console.log("MARKER SEPERATOR");
+    }
+  });
+
+});
